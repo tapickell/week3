c54f74ab619fd7c3bdd9dee209b60956a983eac2
Todd Pickell <tapickell@gmail.com>
2018-02-06 12:53:35 -0800

    stuff from class

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Vairable Assignement Code
+var camelCase = "camelCase";
+var snake_case = "snake_case";
+var count = 42;
+var is_true = true;
 
+var cc = camelCase;
+console.log(camelCase);
+console.log(cc);
+var stuff = count + 1;
+console.log(stuff);
+var answer = "The answer is ";
+console.log(answer + count);
 
+console.log(camelCase, snake_case, count, is_true)
 // People as Code Variables
 
 
